9b77006cf6aee71e24d76f563c71d06582ee096a
Erik Hellman <iam@erikhellman.com>
2017-08-18 13:28:27 +0200

    add jsonp response

--- server.js
+++ server.js
@@ -1,15 +1,18 @@
 const path = require('path')
+const fs = require('fs')
 const jsonServer = require('json-server')
 const cors = require('cors')
 const express = require('express')
-// const server = jsonServer.create()
-// const router = jsonServer.router(path.join(__dirname, 'db.json'))
-// const middlewares = jsonServer.defaults()
+const bodyParser = require('body-parser')
 
 const server = express()
+
 server.use(cors())
+server.use(bodyParser.urlencoded({ extended: true}));
+server.use(bodyParser.json());
 server.use('/images', express.static(path.join(__dirname, 'images')))
-server.use('/', jsonServer.router('db.json'))
-// server.use(middlewares)
-// server.use(router)
+server.use('/', (req, res) => {
+  res.json(require('./db.json'))
+})
+
 server.listen(3636)
