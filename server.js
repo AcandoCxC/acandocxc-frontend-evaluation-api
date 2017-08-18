aba85e69f3080f6c3345dc6b7a0f146d1baae47d
Erik Hellman <iam@erikhellman.com>
2017-08-18 13:48:42 +0200

    Add /products endpoint

--- server.js
+++ server.js
@@ -1,6 +1,4 @@
 const path = require('path')
-const fs = require('fs')
-const jsonServer = require('json-server')
 const cors = require('cors')
 const express = require('express')
 const bodyParser = require('body-parser')
@@ -11,8 +9,6 @@ server.use(cors())
 server.use(bodyParser.urlencoded({ extended: true}));
 server.use(bodyParser.json());
 server.use('/images', express.static(path.join(__dirname, 'images')))
-server.use('/', (req, res) => {
-  res.json(require('./db.json'))
-})
+server.use('/products', (req, res) => res.json(require('./db.json')))
 
 server.listen(3636)
